928176e7bce8bb4fbf273929318bbcce1909de78
Kyeongrok <oceanfog1@gmail.com>
2022-10-24 18:25:30 +0900

    HashFunction

--- src/main/java/com/likelion/hash/HashFunction.java
+++ src/main/java/com/likelion/hash/HashFunction.java
@@ -1,17 +1,34 @@
 package com.likelion.hash;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class HashFunction {
+    int[] table = new int[1000];
     public int hash(String str) {
         int ascii = 0;
         for (int i = 0; i < str.length(); i++) {
             ascii += str.charAt(i);
         }
-        return ascii / 1000;
+        return ascii % 1000;
+    }
+
+    public void insert(String key, int value) {
+        this.table[hash(key)] = value;
+    }
+
+    public int get(String key) {
+        return this.table[hash(key)];
     }
 
     public static void main(String[] args) {
         HashFunction hf = new HashFunction();
-        int i = hf.hash("kyeonrok");
-        System.out.println(i);
+        hf.insert("kyeongrok", 10);
+        if (10 == hf.get("kyeongrok")) {
+            System.out.println("테스트 통과");
+        }
+
     }
 }
