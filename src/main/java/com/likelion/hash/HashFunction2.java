70e884b97130b90266f998ea008f46ec43c9dba8
Kyeongrok <oceanfog1@gmail.com>
2022-10-25 10:37:47 +0900

    HashFunction

---
+++ src/main/java/com/likelion/hash/HashFunction2.java
@@ -0,0 +1,15 @@
+package com.likelion.hash;
+
+public class HashFunction2 {
+    public int hash(String key) {
+        int asciiSum = 0;
+        for (int i = 0; i < key.length(); i++) {
+            asciiSum += key.charAt(i);
+        }
+        return asciiSum % 90;
+    }
+    public static void main(String[] args) {
+        HashFunction2 hf = new HashFunction2();
+        hf.hash("kyeongrok");
+    }
+}
