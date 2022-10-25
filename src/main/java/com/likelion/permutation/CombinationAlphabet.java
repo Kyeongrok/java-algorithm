28ac33bdc1b16a42d39537f51cd78f8f5cd0609a
Kyeongrok <oceanfog1@gmail.com>
2022-10-25 22:22:35 +0900

    알파벳 조합하기

---
+++ src/main/java/com/likelion/permutation/CombinationAlphabet.java
@@ -0,0 +1,17 @@
+package com.likelion.permutation;
+
+public class CombinationAlphabet {
+    public void printStrings(int n, String prefix) {
+        if(n == 0){
+            System.out.println(prefix);
+        } else {
+            for (int i = 0; i < 26; i++) {
+                printStrings(n - 1, prefix + (char) (i + 65));
+            }
+        }
+    }
+    public static void main(String[] args) {
+        CombinationAlphabet combinationAlphabet = new CombinationAlphabet();
+        combinationAlphabet.printStrings(2, "");
+    }
+}
