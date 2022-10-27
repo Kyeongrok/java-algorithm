ac5b45d2b4cbdd73c42c737470950d7a2fe38416
Kyeongrok <oceanfog1@gmail.com>
2022-10-27 18:35:04 +0900

    전화번호부 목록

--- src/main/java/com/likelion/hash/PhoneNumListArray.java
+++ src/main/java/com/likelion/hash/PhoneNumListArray.java
@@ -5,15 +5,15 @@ import java.util.Set;
 
 public class PhoneNumListArray {
     public boolean solution(String[] phone_book) {
-
+        // 통과는 하지만 속도에서 틀린다
         for (int i = 0; i < phone_book.length; i++) {
             int cnt = 0;
             for (int j = 0; j < phone_book.length; j++) {
-                if (phone_book[i].contains(phone_book[j])) {
+                if (phone_book[i].indexOf(phone_book[j]) == 0) {
                     cnt += 1;
                 }
+                if(cnt > 1) return false;
             }
-            if(cnt > 1) return false;
         }
         return true;
     }
