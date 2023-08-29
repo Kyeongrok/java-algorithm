20630ee6860931728c35f9a22068d5ea01d5c582
Kyeongrok <oceanfog1@gmail.com>
2023-08-30 01:27:54 +0900

    Codeup1675

---
+++ src/main/java/com/likelion/codeup/Codeup1272.java
@@ -0,0 +1,32 @@
+package com.likelion.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1272 {
+    /*
+    1   2  3  4   5  6   7  8
+    1, 10, 2, 20, 3, 30, 4, 40, 5, 50, 6, 60
+    홀수면 1 2 3
+    짝수면 10 20 30
+
+    81 82 83 84
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        for (int i = 0; i < 2; i++) {
+            int k = sc.nextInt();
+            if (k % 2 != 0){
+                // 홀수
+//                System.out.printf("%d ", k / 2 + 1);
+                sum += k / 2 + 1;
+            } else {
+                // 짝수
+//                System.out.printf("%d ", k * 5);
+                sum += k * 5;
+            }
+        }
+        // 2161
+        System.out.println(sum);
+    }
+}
