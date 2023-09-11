c0dfeefb106135597fb74f1ffd5ee71f96213892
Kyeongrok <oceanfog1@gmail.com>
2023-09-11 16:22:33 +0900

    FEAT: Codeup2102 - 배수

--- src/main/java/com/likelion/codeup/dfsbfs/Codeup2102.java
+++ src/main/java/com/likelion/codeup/dfsbfs/Codeup2102.java
@@ -6,14 +6,23 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Codeup2102 {
-    // https://codeup.kr/problem.php?id=2102
-
+    /*
+    배수 (Hard)
+    https://codeup.kr/problem.php?id=2102
+     */
+    public static BigInteger boolToBigint(boolean[] ba) {
+        String str = "";
+        for (int i = 0; i < ba.length; i++) {
+            str = ba[i]?"1":"0" + str;
+        }
+        return new BigInteger(str);
+    }
     public static void bfs(Queue<String> queue, String targetNum) {
         Queue<String> queue2 = new LinkedList<>();
         while (!queue.isEmpty()) {
             BigInteger n = new BigInteger(queue.poll());
-            System.out.println(n);
-            if(!n.mod(new BigInteger(targetNum)).equals(0)){
+//            System.out.println(n);
+            if(n.mod(new BigInteger(targetNum)).intValue() != 0){
                 queue2.add(n + "0");
                 queue2.add(n + "1");
             } else {
@@ -23,14 +32,17 @@ public class Codeup2102 {
         }
         bfs(queue2, targetNum);
     }
+
     public static void main(String[] args) {
-//        int targetNum = 3;
-        Scanner sc = new Scanner(System.in);
+        String targetNum = "396";
+//        Scanner sc = new Scanner(System.in);
 //        String targetNum = sc.nextLine();
         Queue<String> queue = new LinkedList<>();
         queue.add("1");
-//        bfs(queue, targetNum);
+        bfs(queue, targetNum);
         // ToDo: 미완성k
+
+//        System.out.println(new BigInteger("111").mod(new BigInteger("3")));
 
     }
 }
