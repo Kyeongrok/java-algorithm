dbc22c7f8a1dc374899db7c921a877fa2af5e22b
kyeongrok.kim <kyeongrok.kim@e3view.com>
2024-01-07 00:14:44 +0900

    C1201

---
+++ src/main/java/com/likelion/codeup/C1144.java
@@ -0,0 +1,12 @@
+package com.likelion.codeup;
+
+import java.util.Scanner;
+
+public class C1144 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        boolean r = sc.nextInt() != 1;
+        System.out.println(r?1:0);
+    }
+}
