f05f7e1a011c5934a07e31f0d68a2b457a27e79e
kyeongrok.kim <kyeongrok.kim@e3view.com>
2024-01-06 17:54:22 +0900

    FEAT: Codeup1165

---
+++ src/main/java/com/likelion/codeup/Codeup1125.java
@@ -0,0 +1,13 @@
+package com.likelion.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1125 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+
+        System.out.printf("%o %X\n", i, i);
+    }
+}
---
+++ src/main/java/com/likelion/codeup/Codeup1128.java
@@ -0,0 +1,11 @@
+package com.likelion.codeup;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Codeup1128 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(BigDecimal.valueOf(123456789).multiply(BigDecimal.valueOf(sc.nextInt())));;
+    }
+}
