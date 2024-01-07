c9baecec70e196c351c917c07e377bf299437040
kyeongrok.kim <kyeongrok.kim@e3view.com>
2024-01-07 17:14:41 +0900

    1218

---
+++ src/main/java/com/likelion/codeup/C1204.java
@@ -0,0 +1,22 @@
+package com.likelion.codeup;
+
+import java.util.Scanner;
+
+public class C1204 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        int oneThDec = i % 10;
+
+        String postFix = "th";
+        switch (oneThDec){
+            case 1: postFix = "st"; break;
+            case 2: postFix = "nd"; break;
+            case 3: postFix = "rd"; break;
+        }
+        if(i / 10 == 1) postFix = "th";
+
+        System.out.printf("%d%s\n", i, postFix);
+    }
+}
---
+++ src/main/java/com/likelion/codeup/C1216.java
@@ -0,0 +1,21 @@
+package com.likelion.codeup;
+
+import java.util.Scanner;
+
+public class C1216 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        int j = sc.nextInt();
+        int k = sc.nextInt();
+
+        if (i < (j - k)) {
+            System.out.println("advertise");
+        } else if (i > (j - k)) {
+            System.out.println("do not advertise");
+        } else {
+            System.out.println("does not matter");
+        }
+    }
+}
